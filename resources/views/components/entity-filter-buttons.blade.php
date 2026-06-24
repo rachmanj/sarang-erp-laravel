b55c792a68a84529f0fe520f96f4281947308449
rachmanj <rachmanj@gmail.com>
2026-06-24 11:09:52 +0800

    fix entity selection

--- resources/views/components/entity-filter-buttons.blade.php
+++ resources/views/components/entity-filter-buttons.blade.php
@@ -1,17 +1,30 @@
 @props(['name' => 'entity_filter'])
 
+@php
+    $entities = \App\Models\CompanyEntity::query()
+        ->where(function ($query) {
+            $query->where('is_active', true)->orWhereNull('is_active');
+        })
+        ->orderBy('code')
+        ->get(['id', 'code', 'name']);
+
+    $entityShortLabel = static function (\App\Models\CompanyEntity $entity): string {
+        return match ($entity->code) {
+            '71' => 'PT CSJ',
+            '72' => 'CV Saranghae',
+            default => $entity->name,
+        };
+    };
+@endphp
+
 <div {{ $attributes->merge(['class' => 'btn-group btn-group-sm btn-group-toggle']) }} data-toggle="buttons">
     <label class="btn btn-outline-secondary active mb-0">
         <input type="radio" name="{{ $name }}" id="entity-all" value="" checked> All
     </label>
-    @if ($ptCahaya ?? null)
+    @foreach ($entities as $entity)
         <label class="btn btn-outline-secondary mb-0">
-            <input type="radio" name="{{ $name }}" id="entity-pt" value="{{ $ptCahaya->id }}"> PT CSJ
+            <input type="radio" name="{{ $name }}" id="entity-{{ $entity->id }}" value="{{ $entity->id }}">
+            {{ $entityShortLabel($entity) }}
         </label>
-    @endif
-    @if ($cvCahaya ?? null)
-        <label class="btn btn-outline-secondary mb-0">
-            <input type="radio" name="{{ $name }}" id="entity-cv" value="{{ $cvCahaya->id }}"> CV Saranghae
-        </label>
-    @endif
+    @endforeach
 </div>
